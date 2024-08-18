f1b44f56357696fad303162dafb2fd9889d1a17a
mnergizci <83592714+mnergizci@users.noreply.github.com>
2024-08-18 14:01:15 +0100

    Update runInversion.m
    standart_format

--- GBIS/RunInversion/runInversion.m
+++ GBIS/RunInversion/runInversion.m
@@ -32,9 +32,11 @@ model.range = model.upper - model.lower; % Calculate range of m parameters
 
 % Check that starting model is within bounds
 if sum(model.m > model.upper)>0 || sum(model.m < model.lower)>0
-    disp('      Parameter#  Lower_Bound  Sart_Model  Upper_Bound')
+    disp('Parameter#  Lower_Bound  Sart_Model  Upper_Bound')
     ix = find(model.m > model.upper | model.m < model.lower);
-    fprintf('%d %f6 %f6 %f6', ix, model.lower(ix), model.m(ix), model.upper(ix))
+    for i=1:length(ix)
+        fprintf('    %d      %f %f %f\n', ix(i), model.lower(ix(i)), model.m(ix(i)), model.upper(ix(i)))
+    end
     error('Starting model is out of bounds')
 end
 
@@ -55,6 +57,49 @@ T = invpar.TSchedule(1); % Set first temperature
 iTemp = 0;  % Initialise temperature schedule index (for initial Simulated Annealing)
 nTemp = length(invpar.TSchedule); % Number of temperature steps (for initial Simulated Annealing)
 
+i_mdike=0;
+i_mdisloc=0;
+for i = 1:invpar.nModels % For each source model...
+    index1 = model.mIx(i);
+    switch invpar.model{i}
+        case 'MDIK'
+            i_mdike=i_mdike+1;
+            mdike=load(modelInput.mdike{i_mdike}.patchfile);
+            mdike.m([8,9],:)=0; % Set slip to 0
+            mdike.m(10,:)=1; % Set openning to 1
+            mFunc{i}=mdike.m;
+            for i2 = 1:size(mdike.m,2)
+                Uunit{i}{i2} = disloc(mdike.m(:,i2),obs(1:2,:),nu); % displacements for unit slip
+            end
+        case 'MDLC'
+            i_mdisloc=i_mdisloc+1;
+            mdisloc=load(modelInput.mdisloc{i_mdisloc}.patchfile);
+            mdisloc.m([8:10],:)=0; % Set slip and opening to 0
+            mFunc{i}=mdisloc.m;
+            ssdsop{i}=modelInput.mdisloc{i_mdisloc}.ss_ds_op;
+            if ssdsop{i}(1)
+                mdisloc.m(8,:)=1; % Set ss to 1
+                for i2 = 1:size(mdisloc.m,2)
+                    Uunit{i}{1}{i2} = disloc(mdisloc.m(:,i2),obs(1:2,:),nu); % displacements for unit slip
+                end
+                mdisloc.m(8,:)=0; % Set ss to 0
+            end
+            if ssdsop{i}(2)
+                mdisloc.m(9,:)=1; % Set ds to 1
+                for i2 = 1:size(mdisloc.m,2)
+                    Uunit{i}{2}{i2} = disloc(mdisloc.m(:,i2),obs(1:2,:),nu); % displacements for unit slip
+                end
+                mdisloc.m(9,:)=0; % Set ds to 0
+            end
+            if ssdsop{i}(3)
+                mdisloc.m(10,:)=1; % Set openning to 1
+                for i2 = 1:size(mdisloc.m,2)
+                    Uunit{i}{3}{i2} = disloc(mdisloc.m(:,i2),obs(1:2,:),nu); % displacements for unit slip
+                end
+            end
+    end
+end
+                
 %% Start core of inversion
 
 sensitivityTest = 0; % Switch off sensitivity test at first iteration
@@ -81,6 +126,7 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
     %% Calculate 3D displacements from model
     
     UTot = zeros(3,nObs);   % Initialise matrix of modeled displacements (3 x number of observation points)
+    Uinsar = cell(1,max(length(insar),length(gps)));
     for i = 1:invpar.nModels % For each source model...
         index1 = model.mIx(i);
         switch invpar.model{i}
@@ -105,16 +151,68 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
             case 'FAUL'
                 mFunc{i}=[model.trial(index1:index1+8);0]; % Select source model parameters from all; Opening set to 0;
                 U = disloc(mFunc{i},obs(1:2,:),nu); 
+            case 'DLOC'
+                mFunc{i}=[model.trial(index1:index1+9)]; % Select source model parameters from all; Opening set to 0;
+                U = disloc(mFunc{i},obs(1:2,:),nu); 
+            case 'MDIK'
+                U=zeros(3,size(obs,2));
+                for i2=1:size(Uunit{i},2)
+                    U = U + Uunit{i}{i2}*model.trial(index1+i2-1); 
+                end
+                mFunc{i}(10,:)=model.trial(index1:index1+i2-1);
+             case 'MDLC'
+                U=zeros(3,size(obs,2));
+                Np=size(mFunc{i},2); % number of patches
+                i_inc=0;
+                for i2=1:3
+                    if ssdsop{i}(i2)
+                        for i3=1:Np
+                            U = U + Uunit{i}{i2}{i3}*model.trial(index1+i_inc*Np+i3-1); 
+                                                  
+                        end
+                        mFunc{i}(7+i2,:)=model.trial(index1+i_inc*Np:index1+(i_inc+1)*Np-1);
+                        i_inc=i_inc+1;
+                    end
+                end
             case 'HING'
                 mFunc{i}=model.trial(index1:index1+10);     % Select source model parameters from all
                 U = hingedDikes(mFunc{i},obs(1:2,:),nu);    % Calculate 3D displacements
+            case 'PCDM'
+                mFunc{i}=model.trial(index1:index1+8); 
+                % Calculate 3D displacements using Nikkhoo's code
+                [ue,un,uv] = pCDM(obs(1,:),obs(2,:),mFunc{i}(1,:),mFunc{i}(2,:),mFunc{i}(3,:), ...
+                    mFunc{i}(4,:),mFunc{i}(5,:),mFunc{i}(6,:),mFunc{i}(7,:),mFunc{i}(8,:),mFunc{i}(9,:),nu);
+                U=[ue';un';uv'];
+            case 'CDM'
+                mFunc{i}=model.trial(index1:index1+9); 
+                % Calculate 3D displacements using Nikkhoo's code
+                [ue,un,uv] = CDM(obs(1,:),obs(2,:),mFunc{i}(1,:),mFunc{i}(2,:),mFunc{i}(3,:), ...
+                    mFunc{i}(4,:),mFunc{i}(5,:),mFunc{i}(6,:),mFunc{i}(7,:),mFunc{i}(8,:),mFunc{i}(9,:),mFunc{i}(10,:),nu);
+                U=[ue';un';uv'];
+            case 'FECM'
+                mu = modelInput.mu; % shear modulus
+                lambda=2*mu*nu/(1-2*nu); % Lamé first constant
+                mFunc{i}=model.trial(index1:index1+9);
+                % Calculate 3D displacements using Nikkhoo's code
+                [ue,un,uv,dV,DV,Ns] = fECM(xy(1,:),xy(2,:),mFunc{i}(1,:),mFunc{i}(2,:),mFunc{i}(3,:),mFunc{i}(4,:),mFunc{i}(5,:), ...
+                    mFunc{i}(6,:),mFunc{i}(7,:),mFunc{i}(8,:),mFunc{i}(9,:),mFunc{i}(10,:),mu,lambda,'C');
+                U=[ue';un';uv'];       
         end
-        UTot = UTot + U; % Calculate total displacement from sum of displacement from each source
+        if isempty(invpar.modelinsarID{i})
+            UTot = UTot + U; % Calculate total displacement from sum of displacement from each source
+        else
+            for i2=1:length(invpar.modelinsarID{i}) % only add for selected interferograms
+                if isempty(Uinsar{invpar.modelinsarID{i}(i2)})
+                    Uinsar{invpar.modelinsarID{i}(i2)}= U;
+                else
+                    Uinsar{invpar.modelinsarID{i}(i2)}=Uinsar{invpar.modelinsarID{i}(i2)} + U;
+                end
+            end
+        end
     end
     
     insarParIx = model.mIx(end); % Identify first model parameter not related to source model (e.g., InSAR offset, ramp, etc.)
     
-    U = UTot; % Reassign U to Utot for simplicity
     
     %% Convert 3D displacement to LOS displacement and calculate residuals
     resExp = 0; % Initialise (Gm - d) * InvCov * (Gm - d)'
@@ -124,6 +222,10 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
         ULos = []; % Initialise line-of-sight Gm vector
         
         for j = 1 : length(insar)
+            U=UTot;
+            if ~isempty(Uinsar{j})
+                U=U+Uinsar{j};
+            end
             UEast = -cosd(insar{j}.dHeading).* sind(insar{j}.dIncidence); % East unit vector
             UNorth = sind(insar{j}.dHeading).* sind(insar{j}.dIncidence); % North unit vector
             UVert = cosd(insar{j}.dIncidence); % Vertical unit vector
@@ -154,8 +256,17 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
     %% Calculate GPS residuals
     
     if ~isempty(gps)
-        rGps = U(1:3,gps.ix) - gps.displacements(1:3,:); % Residual GPS displacement
-        resExp = resExp + rGps(:)' * gps.invCov * rGps(:) * gps.weight; % (Gm - d) * InvCov * (Gm - d)'
+        for j = 1 : length(gps)
+            if ~isempty(gps{j})
+                U=UTot(1:3,gps{1}.ix);      
+                if ~isempty(Uinsar{j})
+                    U=U+Uinsar{j}(1:3,gps{1}.ix);
+                end
+                rGps = U - gps{j}.displacements(1:3,:); % Residual GPS displacement
+                nonanix=~isnan(rGps(:));
+                resExp = resExp + rGps(nonanix)' * gps{j}.invCov(nonanix,nonanix) * rGps(nonanix) * gps{j}.weight; % (Gm - d) * InvCov * (Gm - d)'
+            end
+        end
     end
     
     %% Continue inversion ...
@@ -215,10 +326,10 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
             PPrev = P;  % Assign current P to PPrev for next trial
             hyperPrev = hyperParam; % Assign current Hyperparameter for next trial
             
-            if -resExp > POpt   % Update current optimal model if likelihood is higher
+            if P > POpt   % Update current optimal model if likelihood is higher
                 model.optimal = model.m;
                 model.funcOpt = mFunc;
-                POpt = -resExp;
+                POpt = P;
             end
         else                    % Reject model trial and keep previous model
             iReject = iReject + 1;
@@ -249,12 +360,12 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
             % Display current optimal model parameters on screen
             for i=1:length(invpar.model)
                 if invpar.model{i} == 'MOGI'
-                    fprintf('MOGI center X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nMOGI center X: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('MOGI center Y: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('MOGI depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('MOGI volume change: %f\n',(model.funcOpt{i}(4,:)));
                 elseif invpar.model{i} == 'YANG'
-                    fprintf('YANG centroid X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nYANG centroid X: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('YANG centroid Y: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('YANG centroid depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('YANG major axis: %f\n',(model.funcOpt{i}(4,:)));
@@ -263,19 +374,19 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
                     fprintf('YANG majax plunge: %f\n',(model.funcOpt{i}(7,:)));
                     fprintf('YANG DP/mu: %f\n',(model.funcOpt{i}(8,:)));
                 elseif invpar.model{i} == 'MCTG'
-                    fprintf('MCTG center X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nMCTG center X: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('MCTG center Y: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('MCTG center depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('MCTG radius: %f\n',(model.funcOpt{i}(4,:)));
                     fprintf('MCTG DP/mu: %f\n',(model.funcOpt{i}(5,:)));
                 elseif invpar.model{i} == 'PENN'
-                    fprintf('PENNY center X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nPENNY center X: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('PENNY center Y: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('PENNY center depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('PENNY radius: %f\n',(model.funcOpt{i}(4,:)));
                     fprintf('PENNY DP/mu: %f\n',(model.funcOpt{i}(5,:)));
                 elseif invpar.model{i} == 'SILL'
-                    fprintf('SILL length: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nSILL length: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('SILL width: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('SILL depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('SILL strike: %f\n',(model.funcOpt{i}(5,:)));
@@ -283,7 +394,7 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
                     fprintf('SILL edge center Y: %f\n',(model.funcOpt{i}(7,:)));
                     fprintf('SILL opening: %f\n',(model.funcOpt{i}(10,:)));
                 elseif invpar.model{i} == 'DIKE'
-                    fprintf('DIKE length: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nDIKE length: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('DIKE width: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('DIKE depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('DIKE dip: %f\n',(model.funcOpt{i}(4,:)));
@@ -292,7 +403,7 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
                     fprintf('DIKE edge center Y: %f\n',(model.funcOpt{i}(7,:)));
                     fprintf('DIKE opening: %f\n',(model.funcOpt{i}(10,:)));
                 elseif invpar.model{i} == 'FAUL'
-                    fprintf('FAULT length: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nFAULT length: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('FAULT width: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('FAULT depth: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('FAULT dip: %f\n',(model.funcOpt{i}(4,:)));
@@ -301,18 +412,67 @@ while iKeep < invpar.nRuns  % While number of iterations is < than number of run
                     fprintf('FAULT edge center Y: %f\n',(model.funcOpt{i}(7,:)));
                     fprintf('FAULT strike-slip component: %f\n',(model.funcOpt{i}(8,:)));
                     fprintf('FAULT dip-slip component: %f\n',(model.funcOpt{i}(9,:)));
+                elseif invpar.model{i} == 'DLOC'
+                    fprintf('\nDISLOCATION length: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('DISLOCATION width: %f\n',(model.funcOpt{i}(2,:)));
+                    fprintf('DISLOCATION depth: %f\n',(model.funcOpt{i}(3,:)));
+                    fprintf('DISLOCATION dip: %f\n',(model.funcOpt{i}(4,:)));
+                    fprintf('DISLOCATION strike: %f\n',(model.funcOpt{i}(5,:)));
+                    fprintf('DISLOCATION edge center X: %f\n',(model.funcOpt{i}(6,:)));
+                    fprintf('DISLOCATION edge center Y: %f\n',(model.funcOpt{i}(7,:)));
+                    fprintf('DISLOCATION strike-slip component: %f\n',(model.funcOpt{i}(8,:)));
+                    fprintf('DISLOCATION dip-slip component: %f\n',(model.funcOpt{i}(9,:)));
+                    fprintf('DISLOCATION opening: %f\n',(model.funcOpt{i}(10,:)));
+                elseif invpar.model{i} == 'MDIK'
+                    fprintf('\nMULTI-PATCH DIKE Opening: %f to %f\n',min(model.funcOpt{i}(10,:)),max(model.funcOpt{i}(10,:)));    
+                elseif invpar.model{i} == 'MDLC'
+                    fprintf('\nMULTI-PATCH Dislocation Strike-slip: %f to %f\n',min(model.funcOpt{i}(8,:)),max(model.funcOpt{i}(8,:)));  
+                    fprintf('MULTI-PATCH Dislocation Dip-slip: %f to %f\n',min(model.funcOpt{i}(9,:)),max(model.funcOpt{i}(9,:)));  
+                    fprintf('MULTI-PATCH Dislocation Opening: %f to %f\n',min(model.funcOpt{i}(10,:)),max(model.funcOpt{i}(10,:)));    
                 elseif invpar.model{i} == 'HING'
-                    fprintf('DIKE1 edge center X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('\nDIKE1 edge center X: %f\n',(model.funcOpt{i}(1,:)));
                     fprintf('DIKE1 edge center Y: %f\n',(model.funcOpt{i}(2,:)));
                     fprintf('DIKE1 length: %f\n',(model.funcOpt{i}(3,:)));
                     fprintf('DIKE1 width: %f\n',(model.funcOpt{i}(4,:)));
                     fprintf('DIKE1 depth: %f\n',(model.funcOpt{i}(5,:)));
                     fprintf('DIKE1 dip: %f\n',(model.funcOpt{i}(6,:)));
-                    fprintf('DIKE1 opening: %f\n',(model.funcOpt{i}(7,:)));
+                    fprintf('\nDIKE1 opening: %f\n',(model.funcOpt{i}(7,:)));
                     fprintf('DIKE2 width: %f\n',(model.funcOpt{i}(8,:)));
                     fprintf('DIKE2 dip: %f\n',(model.funcOpt{i}(9,:)));
                     fprintf('DIKE2 opening: %f\n',(model.funcOpt{i}(10,:)));
-                    fprintf('Strike: %f\n',(model.funcOpt{i}(11,:)));
+                    fprintf('Strike: %f\n\n',(model.funcOpt{i}(11,:)));
+                elseif strcmp(invpar.model{i},'PCDM')
+                    fprintf('\nPCDM centroid X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('PCDM centroid Y: %f\n',(model.funcOpt{i}(2,:)));
+                    fprintf('PCDM centroid depth: %f\n',(model.funcOpt{i}(3,:)));
+                    fprintf('PCDM x rotation angle: %f\n',(model.funcOpt{i}(4,:)));
+                    fprintf('PCDM y rotation angle: %f\n',(model.funcOpt{i}(5,:)));
+                    fprintf('PCDM z rotation angle: %f\n',(model.funcOpt{i}(6,:)));
+                    fprintf('PCDM x Potencies of the PTDs: %f\n',(model.funcOpt{i}(7,:)));
+                    fprintf('PCDM y Potencies of the PTDs: %f\n',(model.funcOpt{i}(8,:)));
+                    fprintf('PCDM z Potencies of the PTDs: %f\n',(model.funcOpt{i}(9,:)));
+                elseif strcmp(invpar.model{i},'CDM')
+                    fprintf('\nCDM centroid X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('CDM centroid Y: %f\n',(model.funcOpt{i}(2,:)));
+                    fprintf('CDM centroid depth: %f\n',(model.funcOpt{i}(3,:)));
+                    fprintf('CDM x rotation angle: %f\n',(model.funcOpt{i}(4,:)));
+                    fprintf('CDM y rotation angle: %f\n',(model.funcOpt{i}(5,:)));
+                    fprintf('CDM z rotation angle: %f\n',(model.funcOpt{i}(6,:)));
+                    fprintf('CDM semi-axis x: %f\n',(model.funcOpt{i}(7,:)));
+                    fprintf('CDM semi-axis y: %f\n',(model.funcOpt{i}(8,:)));
+                    fprintf('CDM semi-axis z: %f\n',(model.funcOpt{i}(9,:)));
+                    fprintf('CDM opening: %f\n',(model.funcOpt{i}(10,:)));
+                elseif strcmp(invpar.model{i},'FECM')
+                    fprintf('\nFECM centroid X: %f\n',(model.funcOpt{i}(1,:)));
+                    fprintf('FECM centroid Y: %f\n',(model.funcOpt{i}(2,:)));
+                    fprintf('FECM centroid depth: %f\n',(model.funcOpt{i}(3,:)));
+                    fprintf('FECM x rotation angle: %f\n',(model.funcOpt{i}(4,:)));
+                    fprintf('FECM y rotation angle: %f\n',(model.funcOpt{i}(5,:)));
+                    fprintf('FECM z rotation angle: %f\n',(model.funcOpt{i}(6,:)));
+                    fprintf('FECM semi-axis x: %f\n',(model.funcOpt{i}(7,:)));
+                    fprintf('FECM semi-axis y: %f\n',(model.funcOpt{i}(8,:)));
+                    fprintf('FECM semi-axis z: %f\n',(model.funcOpt{i}(9,:)));
+                    fprintf('FECM pressure: %f\n',(model.funcOpt{i}(10,:)));
                 end
             end
         end
